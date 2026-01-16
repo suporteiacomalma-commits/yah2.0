38ac7b53650e424a29e3b9c85a17d0996a61d29d
Carlosbitencourt <agenciaconativa@gmail.com>
2026-01-15 23:28:46 -0300

    39

--- src/pages/PhasePage.tsx
+++ src/pages/PhasePage.tsx
@@ -77,9 +77,7 @@ export default function PhasePage() {
               <PersonalityNotebook />
             ) : phaseNumber === 2 ? (
               <BrandDNANotebook />
-            ) : phaseNumber === 3 ? (
-              <WeeklyFixedNotebook />
-            ) : phaseNumber === 7 ? (
+            ) : phaseNumber === 5 ? (
               <ActivityCalendar />
             ) : (
               <div className="text-center py-12">
